bba143993a9824f32b2e524bccfc7b52e44774c8
leviegu <gu.lw@outlook.com>
2022-03-21 07:25:21 +0800

    feat(0653): bfs and dfs

---
+++ src/0653-two-sum-iv-input-is-a-bst/resolve.test.ts
@@ -0,0 +1,16 @@
+import { makeTree } from "@/utils/binary-tree-node";
+import "jest";
+import { bfsFindTarget, dfsFindTarget } from "./index";
+
+test("two-sum-iv-input-is-a-bst test", () => {
+  const testExamples = [
+    { root: [5, 3, 6, 2, 4, null, 7], k: 9, ret: true },
+    { root: [5, 3, 6, 2, 4, null, 7], k: 28, ret: false },
+  ];
+
+  testExamples.forEach(({ root, k, ret }) => {
+    const node = makeTree(root);
+    expect(bfsFindTarget(node, k)).toBe(ret);
+    expect(dfsFindTarget(node, k)).toBe(ret);
+  });
+});
